8f3a23ef3c67a8e5f94252c23bff6e62c6e4241d
Jean Brigonnet <jeanbrigo@gmail.com>
2023-04-19 02:11:17 -0500

    added update and delete

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,6 +5,7 @@ const Dashboard = (props) => {
   const { gState, setGState } = React.useContext(GlobalCtx);
   const { url, token } = gState;
   const [notes, setNotes] = React.useState(null);
+  const [updateID, setUpdateID] = React.useState(null);
 
   const getNotes = async () => {
     const response = await fetch(url + "/note/", {
@@ -22,6 +23,7 @@ const Dashboard = (props) => {
   }, []);
 
   const input = React.useRef(null);
+  const update = React.useRef(null);
 
   const handleClick = (event) => {
     const note = input.current.value;
@@ -40,17 +42,64 @@ const Dashboard = (props) => {
       });
   };
 
+  const handleUpdate = () => {
+    const note = update.current.value;
+    fetch(url + "/note/" + updateID, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `bearer ${token}`,
+      },
+      body: JSON.stringify({ note }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        update.current.value = "";
+        setUpdateID(null);
+        getNotes();
+      });
+  };
+
+  const handleDelete = (id) => {
+    fetch(url + "/note/" + id, {
+      method: "delete",
+      headers: {
+        Authorization: `bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        getNotes();
+      });
+  };
+
   return (
     <div>
       <h1>Dashboard</h1>
       <h2>New Note</h2>
       <input type="text" name="note" ref={input} />
       <button onClick={handleClick}>Create Note</button>
+      <h2>Update Note</h2>
+      <input type="text" name="note" ref={update} />
+      <button onClick={handleUpdate}>Update</button>
       <h2>Notes</h2>
       <ul>
-        {notes ? notes.map((note) => (
-          <li key={notes._id}><h3>{note.note}</h3></li>
-        )) : null}
+        {notes
+          ? notes.map((note) => (
+              <li key={notes._id}>
+                <h3>{note.note}</h3>
+                <button onClick={() => handleDelete(note._id)}>Delete</button>
+                <button
+                  onClick={() => {
+                    setUpdateID(note._id);
+                    update.current.value = note.note;
+                  }}
+                >
+                  Edit
+                </button>
+              </li>
+            ))
+          : null}
       </ul>
     </div>
   );
